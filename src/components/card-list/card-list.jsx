ba134fea25452cfb99c3fc4ab60271ee258efbfd
Nishant <108404252+nishantt19@users.noreply.github.com>
2022-12-17 17:37:34 +0530

    Update card-list.jsx

--- src/components/card-list/card-list.jsx
+++ src/components/card-list/card-list.jsx
@@ -4,6 +4,7 @@ import Card from "../card/card";
 import "./card-list.css";
 
 const CardList = (props) => (
+//   using map function, we will be able to show all the cards
   <div className="card-list">
     {props.monsters.map((monster) => (
       <Card key={monster.id} monster={monster} />
